21a8d5acf64bb15327bfc477ddfa939b17cbb14e
U-RICHARDASUS\Richard <richard.axelsson@gmail.com>
2021-05-03 01:12:09 +0200

    Fixed image URLs on the student server

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -6,7 +6,7 @@
 @foreach ($books as $book)
     <div class="book-wrapper">
         <p><strong>{{ $book['title'] }}</strong></p>
-        <img src="/img/{{ $book['image'] }}" class="book-image">
+        <img src="{{ asset('/img/' . $book['image']) }}" class="book-image">
         <p>Author: {{ $book['author'] }}</p>
         <p>ISBN: {{ $book['isbn'] }}</p>
     </div>
