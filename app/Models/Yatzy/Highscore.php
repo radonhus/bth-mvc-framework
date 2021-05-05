0c705de88c679420badda5f717472824cee72920
U-RICHARDASUS\Richard <richard.axelsson@gmail.com>
2021-05-06 01:05:33 +0200

    Minor fix with phpcbf

--- app/Models/Yatzy/Highscore.php
+++ app/Models/Yatzy/Highscore.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
-
 /**
 * The following properties are columns in the table that
 * the model represents (to make phpstan happy)
