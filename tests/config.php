95800a1c74a352225f00b54f2b1a3992bee8b21a
U-RICHARDASUS\Richard <richard.axelsson@gmail.com>
2021-05-06 16:05:30 +0200

    Minor change to config.php to comply with phpcs warning

--- tests/config.php
+++ tests/config.php
@@ -5,10 +5,10 @@
  */
 
 // Get a defined to point at the installation directory
-define("INSTALL_PATH", realpath(__DIR__ . "/.."));
+// define("INSTALL_PATH", realpath(__DIR__ . "/.."));
 
 // Get the autoloader
-require INSTALL_PATH . "/vendor/autoload.php";
+require realpath(__DIR__ . "/..") . "/vendor/autoload.php";
 
 // Include test helpers and mocks
 foreach (glob(__DIR__ . "/Mock/*.php") as $file) {
